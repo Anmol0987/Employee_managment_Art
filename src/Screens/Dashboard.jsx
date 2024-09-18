466223e6a9f844bf84e6a4cac945c5b62a02bc79
Your Name <you@example.com>
2024-09-18 11:58:09 +0530

    icon added

--- src/Screens/Dashboard.jsx
+++ src/Screens/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import  Card  from '../components/Card'
+import Card from '../components/Card'
 import { useRecoilValue } from 'recoil'
 import { CollapsedAtom } from '../atom/Collapsed'
 
@@ -9,12 +9,11 @@ export const Dashboard = () => {
 
     return (
 
-        <div className={`${isCollapsed?'bg-slate-300  w-[95vw] float-end p-16 flex flex-wrap gap-16':'bg-slate-300 flex-wrap  w-[80%] float-end  p-10  flex gap-14'}`}>
+        <div className={`${isCollapsed ? 'bg-slate-300  w-[95vw] float-end p-16 flex flex-wrap gap-16' : 'bg-slate-300 flex-wrap  w-[80%] float-end  p-10  flex gap-14'}`}>
             <Card />
             <Card />
             <Card />
-
-            <Card/>            
+            <Card />
         </div>
 
     )
