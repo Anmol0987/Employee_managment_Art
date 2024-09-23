38e0696e8992b1670b2dd76792f6b272d9ec4352
ayushchhanchar <chhancharayush007@gmail.com>
2024-09-23 15:25:38 +0530

    holiday page

--- src/Screens/Holiday.jsx
+++ src/Screens/Holiday.jsx
@@ -4,6 +4,9 @@ import { CollapsedAtom } from "../atom/Collapsed";
 import SideBar from "../components/SideBar";
 // import HomeNavBar2 from "../NavigationBar/HomeNavBar2";
 import LeaveTrackerNavBar from "../NavigationBar/LeaveTrackerNavBar";
+import { FaCalendarDays } from "react-icons/fa6";
+import { FaAngleLeft } from "react-icons/fa6";
+import { FaAngleRight } from "react-icons/fa6";
 
 
 
@@ -14,11 +17,16 @@ const Holiday = () => {
       <SideBar />
       <LeaveTrackerNavBar />
       <div
-        className={`${isCollapsed ? "bg-slate-300 h-[90vh] w-[95vw] float-end p-16 ": "bg-slate-300  h-[90vh] w-[80%] float-end  px-28  py-14  gap-28"}`}>
-          <div>
-            <div className=""></div>
-            <div></div>
-          </div>
+        className={`${isCollapsed ? "bg-white h-[90vh] w-[95vw] float-end p-16 ": "bg-white  h-[90vh] w-[80%] float-end  px-28  py-14  gap-28"}`}>
+         <div className="h-full flex flex-col justify-center items-center">
+          <img
+            className="h-48 w-48 "
+            src="https://static.zohocdn.com/zp5/people5/images/nodata/no_record.b8bf88a48edee681e9dc20e785deb816.svg"
+            alt=""
+          />
+          <p>No Data Found</p>
+          <button className=" bg-[#0088FF] py-2 border rounded-md text-white px-5 mt-2 ">Add Holidays</button>
+        </div>
       </div>
     </>
   );
